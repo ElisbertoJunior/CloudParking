6ea59be3698e054718854610bd99bd77e74c8d1e
Elisberto Junior <elisberto_jr@hotmail.com>
2023-09-04 10:58:11 -0300

    Refactoring

--- src/main/java/br/com/elisbjr/cloudparking/controllers/ParkingController.java
+++ src/main/java/br/com/elisbjr/cloudparking/controllers/ParkingController.java
@@ -2,6 +2,7 @@ package br.com.elisbjr.cloudparking.controllers;
 
 import br.com.elisbjr.cloudparking.entity.Parking;
 import br.com.elisbjr.cloudparking.services.ParkingService;
+import io.swagger.v3.oas.annotations.StringToClassMapItem;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,37 +15,36 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/parking")
 @Tag(name = "CloudParking", description = "APIs for integration CloudParking")
-@CrossOrigin(origins = "*")
 public class ParkingController {
     @Autowired
     private ParkingService service;
 
     @PostMapping(value = "/save")
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<Parking> save(@RequestBody Parking parking) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(parking));
     }
 
     @GetMapping(value = "bring-all")
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<List<Parking>> findAll() {
         return ResponseEntity.ok().body(service.findAll());
     }
 
     @GetMapping(value = "/{id}")
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<Parking> findById(@PathVariable Long id) {
         return ResponseEntity.ok().body(service.findById(id));
     }
 
     @PutMapping(value = "/{id}")
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<Parking> update(@PathVariable Long id, @RequestBody Parking updateParking) {
         return ResponseEntity.ok().body(service.updateParking(id, updateParking));
     }
 
     @DeleteMapping(value = "/{id}")
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<List<Parking>> delete(@PathVariable Long id) {
         service.delete(id);
         return findAll();
@@ -52,7 +52,7 @@ public class ParkingController {
     }
 
     @PostMapping(value = "/checkout/{id}")
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<Parking> checkOut(@PathVariable Long id){
         return ResponseEntity.ok().body(service.checkOut(id));
     }
